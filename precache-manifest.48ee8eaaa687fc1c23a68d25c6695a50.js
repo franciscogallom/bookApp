81f19233437ef05035c58778f10db4b56b5256e3
franciscogallom <franciscogallomolinuevo@gmail.com>
2020-08-03 01:41:34 -0300

    Updates

---
+++ precache-manifest.48ee8eaaa687fc1c23a68d25c6695a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411daba04caa3b974c5ca1b8b7fc43bd",
+    "url": "/bookApp/index.html"
+  },
+  {
+    "revision": "72f6b7f3a2d024c18232",
+    "url": "/bookApp/static/css/main.3b46690e.chunk.css"
+  },
+  {
+    "revision": "12ea651f2a24e6ea0a76",
+    "url": "/bookApp/static/js/2.7f19dc45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bookApp/static/js/2.7f19dc45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f6b7f3a2d024c18232",
+    "url": "/bookApp/static/js/main.2ff81713.chunk.js"
+  },
+  {
+    "revision": "1eb0936370a718e7bade",
+    "url": "/bookApp/static/js/runtime-main.a16b5094.js"
+  }
+]);
